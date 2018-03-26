39f94348bcb07785dc9f4b6826025441e45b094c
Sammy Akharaz <ketrixz@gmail.com>
2018-03-26 18:01:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Elon"
-var height = "86"
+var height = 86
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(parseInt(height)+2)
+console.log(height+2)
